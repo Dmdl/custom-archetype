fafebdec5c5d9af4a8185d616aca92adf9c2e3ab
lakmal <lakmald@zone24x7.com>
2018-07-16 18:36:27 +0530

    generated maven architype

--- src/main/resources/archetype-resources/src/test/java/MsTemplateApplicationTests.java
+++ src/main/resources/archetype-resources/src/test/java/MsTemplateApplicationTests.java
@@ -1,4 +1,7 @@
-package com.khcbp;
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
